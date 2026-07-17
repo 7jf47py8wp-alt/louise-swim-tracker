43ac5b78ae15ced578ec4fc416e791dfaa2e95f4
7jf47py8wp-alt <7jf47py8wp@privaterelay.appleid.com>
2026-07-17 10:06:13 +0200

    Create pdfImporter.js

---
+++ js/pdfImporter.js
@@ -0,0 +1,50 @@
+async function importPDF(){
+
+const file =
+document.getElementById("pdfUpload").files[0];
+
+
+if(!file){
+alert("Selecteer eerst een PDF");
+return;
+}
+
+
+const data =
+await file.arrayBuffer();
+
+
+const pdf =
+await pdfjsLib.getDocument(data).promise;
+
+
+let text="";
+
+
+for(
+let i=1;
+i<=pdf.numPages;
+i++
+){
+
+const page =
+await pdf.getPage(i);
+
+
+const content =
+await page.getTextContent();
+
+
+text += content.items
+.map(item=>item.str)
+.join(" ");
+
+}
+
+
+document.getElementById("pdfResult")
+.innerHTML =
+"<pre>"+text+"</pre>";
+
+
+}
