1da415a6ad5bf8ac5666c15f9570a8776697b0de
7jf47py8wp-alt <7jf47py8wp@privaterelay.appleid.com>
2026-07-17 10:30:45 +0200

    Update pdfImporter.js

--- js/pdfImporter.js
+++ js/pdfImporter.js
@@ -1,24 +1,53 @@
+async function loadSwimmer(){
+
+
+    let response =
+    await fetch("data/swimmer.json");
+
+
+    return await response.json();
+
+}
+
+
+
+
+
 async function importPDF(){
+
 
 const file =
 document.getElementById("pdfUpload").files[0];
 
 
 if(!file){
+
 alert("Selecteer eerst een PDF");
+
 return;
+
 }
+
+
+
+let swimmer =
+await loadSwimmer();
+
+
 
 
 const data =
 await file.arrayBuffer();
 
 
+
 const pdf =
 await pdfjsLib.getDocument(data).promise;
 
 
+
 let text="";
+
 
 
 for(
@@ -27,55 +56,106 @@ i<=pdf.numPages;
 i++
 ){
 
+
 const page =
 await pdf.getPage(i);
+
 
 
 const content =
 await page.getTextContent();
 
 
+
 text += content.items
 .map(item=>item.str)
-.join(" ");
+.join("\n");
+
 
 }
 
 
+
+
 let competition =
-parseCompetitionText(text);
+parseCompetitionText(
+text,
+swimmer
+);
+
+
+
+
+
+if(!competition.gevonden){
 
 
 document.getElementById("pdfResult")
 .innerHTML =
 
 `
-
-<h3>${competition.naam}</h3>
-
-<p>
-Datum: ${competition.datum}
-</p>
+<h3>❌ Louise niet gevonden</h3>
 
 <p>
-Zwembad: ${competition.zwembad}
+Geen wedstrijdnummer gevonden voor ${swimmer.lastName}.
+</p>
+`;
+
+
+return;
+
+}
+
+
+
+
+
+document.getElementById("pdfResult")
+.innerHTML =
+
+
+`
+
+<h3>✅ Louise gevonden</h3>
+
+<p>
+Wedstrijd:
+${competition.naam}
 </p>
 
-<h4>Programma:</h4>
+
+<p>
+Datum:
+${competition.datum}
+</p>
+
+
+<p>
+Zwembad:
+${competition.zwembad}
+</p>
+
+
+<h4>Zwemnummers:</h4>
 
 <ul>
 
 ${competition.races.map(r=>
+
 `
 <li>
 ${r.discipline}
 </li>
+
 `
+
 ).join("")}
 
 </ul>
 
 `;
+
+
 
 console.log(competition);
 
